c7013bc7670c5e300efe04f1fc322b3f0c4c95e3
abdirahimsacad <abdirahiimsacad@gmail.com>
2023-07-23 07:01:29 -0700

    update

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+
+
+const age = 30
+
+
+
+if(age > 25){
+    console.log("The largest number out of the two numbers is 25")
+}
+
+if(age < 25){
+    console.log("The largest number out of the two numbers is 25")
+} else if(age == 25){
+    console.log("The largest number out of the two numbers is 25")}
